ae0b5e47c336685cb23e0eca5ccaf2baa328528f
iqbalpa <iqbalpahlevi16@gmail.com>
2023-04-25 16:38:53 +0700

    feat: create eyes detector

--- face-detector/main.py
+++ face-detector/main.py
@@ -3,6 +3,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
+eyes_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -16,6 +17,11 @@ while True:
     # draw rectangle around faces
     for x, y, w, h in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0))
+    # detect eyes
+    eyes = eyes_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    # draw rectangle around eyes
+    for x, y, w, h in eyes:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0))
     # show frame
     cv2.imshow("frame", frame)
     # press q to quit
